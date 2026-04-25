469473c5ae6530f93e923f602039980e71c4de58
abdullahameromran <abdullah.omran1010@gmail.com>
2026-04-25 16:08:38 +0300

    ff

--- supabase/migrations/20260425120000_expand_practitioner_billing_and_usage.sql
+++ supabase/migrations/20260425120000_expand_practitioner_billing_and_usage.sql
@@ -387,32 +387,32 @@ as $$
 begin
   return case p_service_needed
     when 'individual_personal_income_tax_returns' then 2
-    when 'individual_sars_debt_assistance' then 6
-    when 'individual_tax_compliance_issues' then 4
-    when 'individual_tax_clearance_certificates' then 4
+    when 'individual_sars_debt_assistance' then 5
+    when 'individual_tax_compliance_issues' then 3
+    when 'individual_tax_clearance_certificates' then 3
     when 'individual_objections_and_disputes' then 7
-    when 'individual_late_return_submissions' then 4
-    when 'individual_tax_number_registration' then 3
-    when 'individual_tax_status_corrections' then 4
+    when 'individual_late_return_submissions' then 2
+    when 'individual_tax_number_registration' then 2
+    when 'individual_tax_status_corrections' then 2
     when 'business_company_income_tax' then 5
     when 'business_vat_registration' then 3
     when 'business_vat_returns' then 4
-    when 'business_paye_registration' then 4
-    when 'business_paye_compliance' then 4
+    when 'business_paye_registration' then 3
+    when 'business_paye_compliance' then 5
     when 'business_sars_debt_arrangements' then 6
-    when 'business_tax_clearance_certificates' then 4
-    when 'business_sars_audits_support' then 5
+    when 'business_tax_clearance_certificates' then 3
+    when 'business_sars_audits_support' then 7
     when 'accounting_bookkeeping' then 5
-    when 'accounting_financial_statements' then 5
-    when 'accounting_management_accounts' then 4
-    when 'accounting_payroll_services' then 4
-    when 'accounting_monthly_accounting_services' then 6
-    when 'accounting_annual_financial_reporting' then 5
-    when 'support_company_registration' then 3
-    when 'support_business_compliance' then 4
-    when 'support_cipc_services' then 3
-    when 'support_business_advisory' then 5
-    when 'support_financial_compliance' then 5
+    when 'accounting_financial_statements' then 6
+    when 'accounting_management_accounts' then 6
+    when 'accounting_payroll_services' then 5
+    when 'accounting_monthly_accounting_services' then 5
+    when 'accounting_annual_financial_reporting' then 6
+    when 'support_company_registration' then 4
+    when 'support_business_compliance' then 5
+    when 'support_cipc_services' then 4
+    when 'support_business_advisory' then 6
+    when 'support_financial_compliance' then 6
     else 4
   end;
 end;
@@ -1609,8 +1609,8 @@ set
   end,
   price_zar = case code
     when 'starter' then 299
-    when 'professional' then 599
-    when 'business' then 999
+    when 'professional' then 499
+    when 'business' then 899
     else price_zar
   end,
   credits_per_month = case code
