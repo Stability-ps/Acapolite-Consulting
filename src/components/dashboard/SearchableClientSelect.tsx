36869d30d23924fd29a98d467d26938efb40c40a
Stability-ps <sandisops@gmail.com>
2026-09-25 21:49:47 +0200

    Fix invoice modal scrolling (#60)
    * Fix dashboard dialog viewport scrolling
    
    * Constrain searchable client dropdown scrolling

--- src/components/dashboard/SearchableClientSelect.tsx
+++ src/components/dashboard/SearchableClientSelect.tsx
@@ -71,10 +71,10 @@ export function SearchableClientSelect({
           <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
         </Button>
       </PopoverTrigger>
-      <PopoverContent className="w-[var(--radix-popover-trigger-width)] p-0" align="start">
+      <PopoverContent className="w-[var(--radix-popover-trigger-width)] max-h-[min(22rem,var(--radix-popover-content-available-height))] overflow-hidden p-0" align="start">
         <Command>
           <CommandInput placeholder={searchPlaceholder} />
-          <CommandList>
+          <CommandList className="max-h-[min(18rem,var(--radix-popover-content-available-height))] overscroll-contain">
             <CommandEmpty>{emptyText}</CommandEmpty>
             <CommandGroup>
               {options.map((option) => {
